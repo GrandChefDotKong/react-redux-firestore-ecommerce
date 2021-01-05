d97a7560a98bef62d6aa3edf7874a3a05e2ec821
GrandChefDotKong <55345801+GrandChefDotKong@users.noreply.github.com>
2021-01-05 19:34:15 +0900

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyBXtb1N1DS1StQGbjMQOL6E0lDURgtfEAE",
-  authDomain: "sushu-shop-react.firebaseapp.com",
-  databaseURL: "https://sushu-shop-react.firebaseio.com",
-  projectId: "sushu-shop-react",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
-  messagingSenderId: "389141864181",
-  appId: "1:389141864181:web:49bcb7a28b2e6ed7c4efbc"
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
